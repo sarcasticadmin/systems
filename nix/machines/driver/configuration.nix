90b8dad96e1b83f9ea1519280a168f1fe3788030
Robert James Hernandez <rob@sarcasticadmin.com>
2022-05-07 23:15:13 -0700

    Adding simple pkg list for essential utils

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -73,11 +73,13 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  #   firefox
-  # ];
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    tmux
+    ag
+   ];
   
   # Enable the OpenSSH daemon.
   services.openssh = {
