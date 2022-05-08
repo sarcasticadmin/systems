ae74f30e85354ad7c78de7f1979a73e842e5d4f0
Robert James Hernandez <rob@sarcasticadmin.com>
2022-05-07 23:09:40 -0700

    Simplify bootstrap and install to /mnt

---
+++ _bootstrap/robs-nixos.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+
+# Ref: https://gist.github.com/mx00s/ea2462a3fe6fdaa65692fe7ee824de3e
+# NixOS install script synthesized from:
+#
+#   - Erase Your Darlings (https://grahamc.com/blog/erase-your-darlings)
+#   - ZFS Datasets for NixOS (https://grahamc.com/blog/nixos-on-zfs)
+#   - NixOS Manual (https://nixos.org/nixos/manual/)
+#
+# It expects the name of the block device (e.g. 'sda') to partition
+# and install NixOS on and an authorized public ssh key to log in as
+# 'root' remotely. The script must also be executed as root.
+#
+# Example: `sudo ./install.sh sde "ssh-rsa AAAAB..."`
+#
+
+set -euo pipefail
+
+################################################################################
+
+export COLOR_RESET="\033[0m"
+export RED_BG="\033[41m"
+export BLUE_BG="\033[44m"
+
+# If disk provisioning goes wrong
+function cleanup {
+  set +e
+  umount /mnt/boot
+  sleep 5
+  zfs destroy -r zroot
+  sleep 5
+  umount /zroot
+  sleep 5
+  zpool destroy zroot
+}
+
+function err {
+    echo -e "${RED_BG}$1${COLOR_RESET}"
+}
+
+function info {
+    echo -e "${BLUE_BG}$1${COLOR_RESET}"
+}
+
+################################################################################
+
+export DISK=$1
+
+if ! [[ -v DISK ]]; then
+    err "Missing argument. Expected block device name, e.g. 'sda'"
+    exit 1
+fi
+
+export DISK_PATH="/dev/${DISK}"
+
+if ! [[ -b "$DISK_PATH" ]]; then
+    err "Invalid argument: '${DISK_PATH}' is not a block special file"
+    exit 1
+fi
+
+if [[ "$EUID" > 0 ]]; then
+    err "Must run as root"
+    exit 1
+fi
+
+# TODO: I dont think I need this
+# personal user name
+USER_NAME="rherna"
+
+AUTHORIZED_SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnoK/IJ1o8bZluhJQsb6q6VOWu25Dc8N4JtmCSZuRXu rherna@sputnik"
+
+export ZFS_POOL="zroot"
+
+# ephemeral datasets
+export ZFS_LOCAL="${ZFS_POOL}/local"
+export ZFS_DS_ROOT="${ZFS_LOCAL}/root"
+export ZFS_DS_NIX="${ZFS_LOCAL}/nix"
+
+# persistent datasets
+export ZFS_SAFE="${ZFS_POOL}/safe"
+export ZFS_DS_HOME="${ZFS_SAFE}/home"
+export ZFS_DS_PERSIST="${ZFS_SAFE}/persist"
+
+export ZFS_BLANK_SNAPSHOT="${ZFS_DS_ROOT}@blank"
+
+################################################################################
+
+info "Running the UEFI (GPT) partitioning and formatting directions from the NixOS manual ..."
+parted "$DISK_PATH" -- mklabel gpt
+parted "$DISK_PATH" -- mkpart primary 512MiB 100%
+parted "$DISK_PATH" -- mkpart ESP fat32 1MiB 512MiB
+parted "$DISK_PATH" -- set 2 boot on
+export DISK_PART_ROOT="${DISK_PATH}p1"
+export DISK_PART_BOOT="${DISK_PATH}p2"
+
+info "Formatting boot partition ..."
+mkfs.fat -F 32 -n boot "$DISK_PART_BOOT"
+
+info "Creating '$ZFS_POOL' ZFS pool for '$DISK_PART_ROOT' ..."
+zpool create -f "$ZFS_POOL" "$DISK_PART_ROOT"
+
+info "Enabling compression for '$ZFS_POOL' ZFS pool ..."
+zfs set compression=on "$ZFS_POOL"
+
+info "Creating '$ZFS_DS_ROOT' ZFS dataset ..."
+zfs create -p -o mountpoint=legacy "$ZFS_DS_ROOT"
+
+info "Configuring extended attributes setting for '$ZFS_DS_ROOT' ZFS dataset ..."
+zfs set xattr=sa "$ZFS_DS_ROOT"
+
+info "Configuring access control list setting for '$ZFS_DS_ROOT' ZFS dataset ..."
+zfs set acltype=posixacl "$ZFS_DS_ROOT"
+
+info "Creating '$ZFS_BLANK_SNAPSHOT' ZFS snapshot ..."
+zfs snapshot "$ZFS_BLANK_SNAPSHOT"
+
+info "Mounting '$ZFS_DS_ROOT' to /mnt ..."
+mount -t zfs "$ZFS_DS_ROOT" /mnt
+
+info "Mounting '$DISK_PART_BOOT' to /mnt/boot ..."
+mkdir /mnt/boot
+mount -t vfat "$DISK_PART_BOOT" /mnt/boot
+
+info "Creating '$ZFS_DS_NIX' ZFS dataset ..."
+zfs create -p -o mountpoint=legacy "$ZFS_DS_NIX"
+
+info "Disabling access time setting for '$ZFS_DS_NIX' ZFS dataset ..."
+zfs set atime=off "$ZFS_DS_NIX"
+
+info "Mounting '$ZFS_DS_NIX' to /mnt/nix ..."
+mkdir /mnt/nix
+mount -t zfs "$ZFS_DS_NIX" /mnt/nix
+
+info "Creating '$ZFS_DS_HOME' ZFS dataset ..."
+zfs create -p -o mountpoint=legacy "$ZFS_DS_HOME"
+
+info "Mounting '$ZFS_DS_HOME' to /mnt/home ..."
+mkdir /mnt/home
+mount -t zfs "$ZFS_DS_HOME" /mnt/home
+
+info "Creating '$ZFS_DS_PERSIST' ZFS dataset ..."
+zfs create -p -o mountpoint=legacy "$ZFS_DS_PERSIST"
+
+info "Mounting '$ZFS_DS_PERSIST' to /mnt/persist ..."
+mkdir /mnt/persist
+mount -t zfs "$ZFS_DS_PERSIST" /mnt/persist
+
+info "Permit ZFS auto-snapshots on ${ZFS_SAFE}/* datasets ..."
+zfs set com.sun:auto-snapshot=true "$ZFS_DS_HOME"
+zfs set com.sun:auto-snapshot=true "$ZFS_DS_PERSIST"
+
+info "Creating persistent directory for host SSH keys ..."
+mkdir -p /mnt/persist/etc/ssh
+
+info "Generating NixOS configuration (/mnt/etc/nixos/*.nix) ..."
+
+# Generate the hardware nix without chroot
+# wont touch configuration.nix if it already exists
+nixos-generate-config --root /mnt
+
+mkdir /mnt/home/${USER_NAME}
+chmod 700 /mnt/home/${USER_NAME}
+
+# Get the right repo and checkout specific branch if needed
+git clone git@github.com:sarcasticadmin/systems.git /mnt/home/${USER_NAME}/systems --branch ${2:-master}
+
+chown 1000:100 -R /mnt/home/${USER_NAME}
+
+# Temporarily make this symlink due to chroot installation needs for config.nix path is consistent before reboot
+ln -s /mnt/home/${USER_NAME} /home/${USER_NAME}
+
+info "Installing NixOS to /mnt ..."
+
+# Great idea: https://discourse.nixos.org/t/github-strategies-for-configuration-nix/1983/20
+cat << EOF > /mnt/etc/nixos/configuration.nix
+{ config, pkgs, options, ... }:
+let
+  machine="driver";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      (/home/${USER_NAME}/systems/nix/machines + "/\${machine}" + /configuration.nix)
+    ];
+}
+EOF
+
+info "need to run nixos-install -I \"nixos-config=/mnt/etc/nixos/configuration.nix\" && passwd bits"
+# nixos-install assumes /mnt as chroot but its nice to be specific here with the configuration.nix path
+nixos-install -I "nixos-config=/mnt/etc/nixos/configuration.nix"
