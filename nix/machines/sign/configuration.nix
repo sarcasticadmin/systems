aa63fc43415d3233541b830d9b9586bfe634a2c2
Robert James Hernandez <rob@sarcasticadmin.com>
2022-10-23 21:18:24 +0000

    Config for sign

---
+++ nix/machines/sign/configuration.nix
@@ -0,0 +1,97 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  # Locals
+in
+{
+  imports =
+    [
+      ../_common/base.nix
+    ];
+
+  # Necessary in most configurations
+  nixpkgs.config.allowUnfree = true;
+
+  # remove the annoying experimental warnings
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "sign"; # Define your hostname.
+  # Need to be set for ZFS or else leads to:
+  # Failed assertions:
+  # - ZFS requires networking.hostId to be set
+  networking.hostId = "7f702d2b";
+
+  # Enables wireless support via wpa_supplicant
+  networking.wireless.enable = false;
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+
+  # Make sure that dhcpcd doesnt timeout when interfaces are down
+  # ref: https://nixos.org/manual/nixos/stable/options.html#opt-networking.dhcpcd.wait
+  networking.dhcpcd.wait = "if-carrier-up";
+  networking.interfaces.enp1s0.useDHCP = true;
+
+  # Enable sound
+  sound.enable = false;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rherna = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "audio" "sound" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEiESod7DOT2cmT2QEYjBIrzYqTDnJLld1em3doDROq" ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    #systemPackages = with pkgs; [
+    #];
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
+  };
+
+  # ZFS
+  services.zfs = {
+    autoScrub = {
+      enable = true;
+      interval = "weekly";
+    };
+    autoSnapshot = {
+      enable = true;
+      monthly = 3;
+    };
+  };
+
+  system.stateVersion = "22.05";
+
+}
