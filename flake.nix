fc54f47683963b9003dcef329ac72fc19414b491
Robert James Hernandez <rob@sarcasticadmin.com>
2022-12-22 21:10:08 +0000

    Update flake input to 22.11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
