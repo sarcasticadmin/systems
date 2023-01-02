020bb9535ad95e34eb159f0ebfc676b6268c99a2
Robert James Hernandez <rob@sarcasticadmin.com>
2023-01-02 09:20:21 +0000

    significant improvement for pinentry prompt
    update-tty-ssh.sh is how Ive been updating my pinentry prompt for ~6
    years. Its crappy and error prone since you have manually call that
    script anytime you need to auth to your yubikey (reboots, unplugging the
    key, etc.)
    
    Thanks to the upstream changes in NixOS for the 22.11 release I no
    longer need to do this:
    https://github.com/NixOS/nixpkgs/commit/576005a34a9b3bbdb0fbe1ce1548fb1fb02fd9ef
    
    Updating the tty for pinentry will now be handled seamlessly!

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -122,10 +122,12 @@ in
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "curses";
+    # Make pinentry across multiple terminal windows, seamlessly
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
