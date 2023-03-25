f7f2700d84608dd48632c6ac5e6570f9f927729f
Robert James Hernandez <rob@sarcasticadmin.com>
2023-03-25 16:00:37 +0000

    ochart plugin almost working just need opengl

--- nix/machines/marco/configuration.nix
+++ nix/machines/marco/configuration.nix
@@ -4,7 +4,37 @@
 
 { config, pkgs, ... }:
 let
-  # Locals
+  # Without it: import plugins seems to fail with: "No GSettings schemas are installed on the system"
+  # https://github.com/NixOS/nixpkgs/issues/16285
+  # https://github.com/NixOS/nixpkgs/pull/220336
+  myOpencpn = pkgs.opencpn.overrideAttrs (old: { buildInputs = old.buildInputs ++ [ pkgs.wrapGAppsHook ]; });
+  oChartsPlugin = pkgs.stdenv.mkDerivation rec {
+    # https://github.com/bdbcat/o-charts_pi.git
+    pname = "o-charts_pi";
+    version = "1.0.34.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "bdbcat";
+      repo = "o-charts_pi";
+      rev = "${version}";
+      hash = "sha256-UP6alDMglvp4tGs1eGMx9uFx5IMDm43NBlTL7em0a4I=";
+    };
+    nativeBuildInputs = with pkgs; [
+      cmake
+      pkg-config
+      gettext
+    ] ++ lib.optionals stdenv.isLinux [
+      lsb-release
+    ];
+    buildInputs = with pkgs; [
+      wxGTK31 # Instead of wxGTK32 due to deprecation errors - maybe try compat in 32 later?
+      tinyxml
+      zlib
+      curl
+    ];
+    cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+    #SEARCH_LIB = "${pkgs.libGLU.out}/lib ${pkgs.libGL.out}/lib";
+  };
 in
 {
   imports =
@@ -27,7 +57,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
+
   networking.hostName = "marco"; # Define your hostname.
   # Need to be set for ZFS or else leads to:
   # Failed assertions:
@@ -76,8 +106,9 @@ in
   environment = {
     systemPackages = with pkgs; [
       newsboat
-      opencpn
       gpsd # explicitly include in pkgs to get gpsd clients: gpsctl, etc
+      myOpencpn
+      oChartsPlugin
     ];
 
     etc."wpa_supplicant.conf" = {
