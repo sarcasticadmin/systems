1dea9d82fcdc6db1a3e33f6999624aee5a097e37
Robert James Hernandez <rob@sarcasticadmin.com>
2023-06-05 00:11:56 +0000

    init ham-overlay as flake input

--- flake.nix
+++ flake.nix
@@ -1,18 +1,29 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      driver = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./nix/machines/driver/configuration.nix ];
-      };
-      sign = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./nix/machines/sign/configuration.nix ];
-        # Example how to pass an arg to configuration.nix:
-        #specialArgs = { hostname = "staging"; };
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    ham-overlay = {
+      url = "github:sarcasticadmin/ham-overlay";
+      # Make sure to set to the specific input of the remote flake
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ham-overlay
+    }: {
+      nixosConfigurations = {
+        driver = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./nix/machines/driver/configuration.nix ];
+        };
+        sign = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./nix/machines/sign/configuration.nix ];
+          # Example how to pass an arg to configuration.nix:
+          #specialArgs = { hostname = "staging"; };
+        };
+      };
+    };
 }
