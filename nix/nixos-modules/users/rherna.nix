f85d6eb10186e3a98876ac75723f06ffec5e11c7
Robert James Hernandez <rob@sarcasticadmin.com>
2026-05-13 02:07:19 +0000

    nix.nixos-modules.users: init rherna

--- nix/nixos-modules/users/rherna.nix
+++ nix/nixos-modules/users/rherna.nix
@@ -1,19 +1,30 @@
-{ pkgs, lib, config, ... }:
+{ lib, config, ... }:
+let
+  cfg = config.sarcasticadmin.users.rherna;
 
+  inherit (lib.modules)
+    mkIf
+    ;
+
+  inherit (lib.options)
+    mkEnableOption
+    ;
+in
 {
-  # allow me to remote nixos-rebuild switch
-  nix.settings.trusted-users = [ "rherna" ];
+  options.sarcasticadmin.users.rherna.enable = mkEnableOption "enable my user";
 
-  users.users.rherna = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = [ "wheel" "dialout" ]
-      ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd"
-      ++ lib.optional config.virtualisation.docker.enable "docker"
-      ++ lib.optional config.programs.light.enable "video"
-      ++ lib.optional config.services.actkbd.enable "input"
-      ++ lib.optionals config.services.pipewire.enable [ "audio" "sound" ]
-      ++ lib.optional (lib.hasAttrByPath [ "plugdev" ] config.users.groups) "plugdev";
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEiESod7DOT2cmT2QEYjBIrzYqTDnJLld1em3doDROq" ];
+  config = mkIf cfg.enable {
+    users.users.rherna = {
+      isNormalUser = true;
+      uid = 1000;
+      extraGroups = [ "wheel" "dialout" ]
+        ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd"
+        ++ lib.optional config.virtualisation.docker.enable "docker"
+        ++ lib.optional config.programs.light.enable "video"
+        ++ lib.optional config.services.actkbd.enable "input"
+        ++ lib.optionals config.services.pipewire.enable [ "audio" "sound" ]
+        ++ lib.optional (lib.hasAttrByPath [ "plugdev" ] config.users.groups) "plugdev";
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEiESod7DOT2cmT2QEYjBIrzYqTDnJLld1em3doDROq" ];
+    };
   };
 }
