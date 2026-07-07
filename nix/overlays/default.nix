c2223ad7e28cb60dd0d6148e2d79d36f1d3333ac
Robert James Hernandez <rob@sarcasticadmin.com>
2026-07-07 01:59:24 +0000

    overlay: wip

---
+++ nix/overlays/default.nix
@@ -0,0 +1,50 @@
+inputs:
+let
+
+  # inherits
+
+  inherit (builtins)
+    attrValues
+    ;
+
+  inherit (inputs.nixpkgs-lib)
+    lib
+    ;
+
+  inherit (lib.attrsets)
+    genAttrs
+    ;
+
+  inherit (lib.customisation)
+    makeScope
+    ;
+
+  inherit (lib.filesystem)
+    packagesFromDirectoryRecursive
+    ;
+
+  inherit (lib.fixedPoints)
+    composeManyExtensions
+    ;
+
+  inherit (inputs.self)
+    library
+    ;
+
+  inherit (library.path)
+    getDirectoryNames
+    joinParentToPaths
+    ;
+
+  # overlays
+
+  toplevelOverlays =
+    final: prev:
+    packagesFromDirectoryRecursive {
+      inherit (final) callPackage;
+      inherit (prev) newScope;
+      directory = ../package-sets/top-level;
+    };
+
+in
+  composeManyExtensions [ toplevelOverlays ]
