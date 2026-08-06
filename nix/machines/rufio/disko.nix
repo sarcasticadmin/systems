ee62f2074a9c2fc3cba1d5caba47234cc6c3ad7a
Robert James Hernandez <rob@sarcasticadmin.com>
2026-08-06 02:12:34 +0000

    nixos-configs.rufio: disko mv to zfs

--- nix/machines/rufio/disko.nix
+++ nix/machines/rufio/disko.nix
@@ -1,30 +1,70 @@
-{ lib, ... }:
 {
   disko.devices = {
     disk = {
-      disk1 = {
-        device = lib.mkDefault "/dev/sda";
+      one = {
         type = "disk";
+        device = "/dev/disk/by-path/pci-0000:01:00.0-nvme-1";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              size = "512M";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-            root = {
+            zfs = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "zfs";
+                pool = "rufio";
               };
             };
+          };
+        };
+      };
+    };
+    zpool = {
+      rufio = {
+        type = "zpool";
+        # Workaround: cannot import 'tank0': I/O error in disko tests
+        options.cachefile = "none";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "true";
+          acltype = "posixacl";
+          xattr = "sa";
+          canmount = "off";
+          mountpoint = "none";
+        };
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "legacy";
+          };
+          "home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options.mountpoint = "legacy";
+          };
+          "nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options.mountpoint = "legacy";
+          };
+          "persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options.mountpoint = "legacy";
           };
         };
       };
